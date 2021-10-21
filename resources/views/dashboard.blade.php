e02389a95acb00c597da20366f7b3f526d1ad215
XuLLg <eduardo.jesus63@gmail.com>
2021-10-21 13:55:44 -0400

    se agrega el proyecto

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Grupo Servigesa Apps <!--Modifico el titulo y sus clases, las cuales estan en tailwind css-->
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                {{-- <x-jet-welcome /> todo lo que ponga aqui cambiara el valor del contenido de la vista principal de jetstream. --}} <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores illum quidem aspernatur soluta corrupti molestias beatae hic assumenda non, id odit pariatur incidunt quibusdam adipisci doloremque dolor nisi, deserunt laboriosam!</p>
+                <p>Ratione vero odit officia, in, cumque et numquam adipisci nostrum iste necessitatibus fugit? Eius, voluptates sit praesentium autem exercitationem mollitia harum? Facere repudiandae quos assumenda dignissimos molestias expedita? Facere, illum!</p>
+                <p>Molestias unde, perspiciatis iure dignissimos a qui culpa ab ipsam similique quaerat veritatis quos ea assumenda voluptatibus voluptates atque! Architecto alias aliquid velit, debitis quia odio saepe itaque aspernatur corporis!</p>
+                <p>Impedit culpa error recusandae? Provident, voluptatibus atque voluptatum libero placeat laudantium distinctio quas quaerat voluptas, obcaecati alias numquam dolorum! Ullam architecto asperiores dolore cumque error facilis animi accusantium. Eaque, maiores?</p>
+                <p>Facilis inventore laborum quidem possimus consequuntur ipsam obcaecati suscipit eveniet quasi quia veritatis, beatae perspiciatis esse error voluptatem ea id odio repellendus sunt laudantium magnam exercitationem? Aut asperiores quibusdam pariatur.</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
